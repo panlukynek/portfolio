85caba321676128e457f6c9bb671009e6062bf70
panlukynek <lukasmolnary@gmail.com>
2025-04-07 23:02:10 +0200

    gotta go fast

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-function intro_overlay_on() {
-    document.getElementById("intro_overlay").style.display = "block";
-    document.body.classList.add('noscroll');
-}
-
-function intro_overlay_off() {
-    document.getElementById("intro_overlay").style.display = "none";
-    document.body.classList.remove('noscroll');
-}
-
-window.addEventListener('scroll', function() {
-    const body = document.body;
-    const intro_text = document.getElementById("intro_text");
-    if (window.scrollY >= window.innerHeight / 2 + intro_text.offsetHeight) {
-        body.style.backdropFilter = "blur(0.5rem)";
-        body.style.backgroundSize = "100%";
-    } else {
-        body.style.backdropFilter = "none";
-        body.style.backgroundSize = "105%";
-    }
+window.addEventListener('scroll', function(){
+    const scrollpos = Math.min(window.scrollY / window.innerHeight, 1);
+    const blur = scrollpos * 0.5;
+    const bgsize = 105 - (scrollpos * 5);
+    
+    document.body.style.backdropFilter = `blur(${blur}rem)`;
+    document.body.style.backgroundSize = `${bgsize}%`;
 });
