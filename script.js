f8b2cce667c85e7f47e1c03a3ea7e80f27987013
panlukynek <lukasmolnary@gmail.com>
2025-05-03 23:31:54 +0200

    im done with ts

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const lenis = new Lenis({
+    autoRaf: true,
+    duration: 0.6,
+});
+
 window.addEventListener('scroll', function(){
     const scrollpos = Math.min(window.scrollY / window.innerHeight, 1);
     const blur = scrollpos * 0.5;
